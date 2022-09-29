34ed277b8e27071749a7e7b683829209f599d227
Vyacheslav <viacheslav87klerik@gmail.com>
2022-09-29 23:14:49 +0300

    Cделано ДЗ - Строки

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задача1
+        String firstName = "Ivan ";
+        String middleName = " Ivanovich ";
+        String lastName = " Ivanov";
+        String fullName = "Ivanov Ivan Ivanovich";
+
+        System.out.println(firstName+middleName+lastName );
+        System.out.println(fullName);
+        //Задача2
+        String fullName1 = fullName.toUpperCase();
+        System.out.println(fullName1);
+        //Задача3
+        fullName = fullName.replace("Ivanov Ivan Ivanovich", "Иванов Семён Семёнович");
+        fullName = fullName.replace("ё","е");
+        System.out.println(fullName);
     }
 }
