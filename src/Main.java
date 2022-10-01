2e09378be2795797873c6ddb8fad8055e0749478
Vyacheslav <viacheslav87klerik@gmail.com>
2022-10-01 13:35:51 +0300

    Исправил замечание.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         //Задача1
-        String firstName = "Ivan ";
-        String middleName = " Ivanovich ";
-        String lastName = " Ivanov";
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
         String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println(firstName+" "+middleName+" "+lastName );
 
-        System.out.println(firstName+middleName+lastName );
-        System.out.println(fullName);
         //Задача2
         String fullName1 = fullName.toUpperCase();
         System.out.println(fullName1);
+
         //Задача3
         fullName = fullName.replace("Ivanov Ivan Ivanovich", "Иванов Семён Семёнович");
         fullName = fullName.replace("ё","е");
